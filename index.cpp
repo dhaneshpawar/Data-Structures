f79f443820cc724d3dc72f503f47102ad531dc96
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-04-22 11:03:28 +0530

    hello world program in C++

---
+++ index.cpp
@@ -0,0 +1,16 @@
+// Command to execute the cpp program
+// clear && g++ index.cpp && ./a.out
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+    typedef int number;
+    number i = 5;
+    cout << "number i = " << i << endl;
+    int k;
+    k = 5;
+    cout << "number k = " << k << endl;
+    return 0;
+}
