79aba4272d5689f519ad03836634f1188092fbf6
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-05-03 11:24:59 +0530

    after this started building array

--- index.cpp
+++ index.cpp
@@ -4,7 +4,12 @@
 #include<iostream>
 using namespace std;
 
+// literal or constant
+// two ways of defining - #define or const where type can be defined
+#define length 23;
+
 int main(){
+    const int mylen = 20;
     cout << "hello world" << endl;
     typedef int number;
     number i = 5;
@@ -12,5 +17,45 @@ int main(){
     int k;
     k = 5;
     cout << "number k = " << k << endl;
+    cout << length;
+    cout << endl;
+    cout << mylen;
+     // local variable declaration:
+   int a = 100;
+   int b = 200;
+   int ret;
+ 
+   // calling a function to get max value.
+   ret = max(a, b);
+   cout << "Max value is : " << ret << endl;
+
+    string me = "hello";
+    cout << me << endl;
+    cout << me.size() << endl;
+
+    for(int i = 0; i < me.size(); i++)
+    {
+        cout << me[i] << endl;
+    }
+
+    cout << endl;
+    char name[3];
+    cout << "Please enter your name : ";
+    cin >> name;
+    cout << "Your name is : " << name << endl; 
+
     return 0;
+}
+
+// function returning the max between two numbers
+int max(int num1, int num2) {
+   // local variable declaration
+   int result;
+ 
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+ 
+   return result; 
 }
