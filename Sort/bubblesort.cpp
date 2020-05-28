13d642f4ce02e2d9d0501a140123f86a88257996
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-05-28 14:27:07 +0530

    made bubble sort performance better

--- Sort/bubblesort.cpp
+++ Sort/bubblesort.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int arr[10] = {10,41,32,63,74,25,56,72,98,19};
+   int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+//    int arr[10] = {10,41,32,63,74,25,56,72,98,19};
    int n=10;
 
    for(int i=0; i<n;i++){cout << arr[i] << " ";}
 
    for(int i =0; i< n;i++){
+           int flag = 0;
        for(int j=0;j<n-i-1;j++)
        {
            if(arr[j] > arr[j+1])
@@ -14,7 +16,12 @@ int main() {
                int tmp = arr[j];
                arr[j] = arr[j+1];
                arr[j+1] = tmp;
+               flag = 1;
            }
+       }
+       if(flag == 0){
+           cout << "this has been breaked";
+           break;
        }
     }
 
